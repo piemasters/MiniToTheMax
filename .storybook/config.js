a466cb98e6097ed4617d1a029ee22ab45467859f
David Norton <pie_masters@hotmail.co.uk>
2020-01-03 21:22:07 +0000

    Added storybook addons with setup

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,40 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters, addDecorator } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import results from '../.jest-results.json';
+import { withTests } from '@storybook/addon-jest';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
+import { withCssResources } from '@storybook/addon-cssresources';
+
+addDecorator(
+  withTests({
+    results,
+  })
+);
+addDecorator(withKnobs);
+addDecorator(withA11y);
+addDecorator(withCssResources);
+
+addParameters({
+  backgrounds: [
+    { name: 'white', value: '#ffffff', default: true },
+    { name: 'black', value: '#000000' },
+    { name: 'twitter', value: '#00aced' },
+    { name: 'facebook', value: '#3b5998' },
+  ],
+  cssresources: [
+    {
+      id: `bluetheme`,
+      code: `<style>body { background-color: lightblue; }</style>`,
+      picked: false,
+    },
+  ],
+  docs: {
+    inlineStories: true,
+    iframeHeight: '60px',
+  },
+  options: { sortStoriesByKind: true },
+});
 
 // automatically import all files ending in *.stories.js
 configure(
