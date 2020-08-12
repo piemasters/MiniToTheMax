b2d70a887efa933c23eae943f2a5f02a5989cd32
David Norton <pie_masters@hotmail.co.uk>
2020-08-12 14:39:13 +0100

    feat: Improve header styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -23,59 +23,54 @@ export const PureHeader = ({ title, logo, pages }: HeaderProps) => {
     padding: 1rem 0 3rem;
   `;
 
-  const NavTitle = css`
-    a {
-      align-items: center;
-      color: ${theme.colors.text};
-      display: flex;
-      padding-bottom: 1rem;
-      text-decoration: none;
-      h1 {
-        margin: 0 0 0 1rem;
-      }
-    }
+  const titleLinkStyle = css`
+    align-items: center;
+    color: ${theme.colors.text};
+    display: flex;
+    padding-bottom: 1rem;
+    text-decoration: none;
   `;
 
-  const NavList = css`
+  const titleStyle = css`
+    margin: 0 0 0 1rem;
+  `;
+
+  const navListStyle = css`
     display: flex;
     list-style-type: none;
     margin: 0;
   `;
 
-  const NavItem = css`
-    a {
-      color: ${theme.colors.textSecondary};
-      font-size: 0.9rem;
-      font-weight: bold;
-      margin-right: 1.3rem;
-      text-decoration: none;
-      &:hover {
-        color: ${theme.colors.primary};
-      }
+  const navItemStyle = css`
+    color: ${theme.colors.textSecondary};
+    font-size: 0.9rem;
+    font-weight: bold;
+    margin-right: 1.3rem;
+    text-decoration: none;
+    &:hover {
+      color: ${theme.colors.primary};
     }
   `;
 
   return (
     <div css={NavHeader} data-testid="nav-header">
       <nav>
-        <div css={NavTitle}>
-          <PageLink type={'paintDrip'} to={'/'}>
-            <img src={logo} alt="Logo" width={60 + 'px'} />
-            <h1>{title} </h1>
-          </PageLink>
-        </div>
-        <div css={NavList}>
+        <PageLink type={'paintDrip'} to={'/'} linkStyle={titleLinkStyle}>
+          <img src={logo} alt="Logo" width={60 + 'px'} />
+          <h1 css={titleStyle}>{title} </h1>
+        </PageLink>
+        <div css={navListStyle}>
           {pages.map((link: NavLink) => {
             return (
-              <div css={NavItem} key={link.name}>
-                <PageLink
-                  type={'cover'}
-                  linkActiveStyle={{ color: theme.colors.primary }}
-                  to={link.url}
-                >
-                  {link.name}
-                </PageLink>
-              </div>
+              <PageLink
+                type={'cover'}
+                linkActiveStyle={{ color: theme.colors.primary }}
+                to={link.url}
+                linkStyle={navItemStyle}
+                key={link.name}
+              >
+                {link.name}
+              </PageLink>
             );
           })}
         </div>
