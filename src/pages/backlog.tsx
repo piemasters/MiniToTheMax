992467014441e9a0ce173bba424a8d09210d575e
David Norton <pie_masters@hotmail.co.uk>
2020-08-18 02:15:07 +0100

    feat: Improved backlog page

--- src/pages/backlog.tsx
+++ src/pages/backlog.tsx
@@ -12,292 +12,155 @@ const BacklogPage = () => {
       />
       <h1>Backlog</h1>
 
-      <h2>40k</h2>
-
-      <h3>Grey Knights</h3>
-      <ul>
-        <li>Imperial Knight</li>
-        <li>Kaldor Draigo</li>
-        <li>Grey Knight Terminators Squad II</li>
-        <li>Grey Knight Paladins</li>
-        <li>Land Raider</li>
-        <li>Grand Master Voldus</li>
-      </ul>
+      <h2>Writing Posts</h2>
+      <hr />
 
       <h3>Space Wolves</h3>
-      <ul>
-        <li>Centurions</li>
-        <li>Wolf Guard Terminators</li>
-        <li>Blood Claws</li>
-        <li>Primaris Captain</li>
-        <li>Primaris Captain</li>
-        <li>Primaris Judicar</li>
-        <li>Primaris Bladeguard Ancient</li>
-        <li>Primaris Intercessor Squad</li>
-        <li>Primaris Inceptor Squad</li>
-        <li>Primaris Hellblaster Squad</li>
-        <li>Primas Infiltrators</li>
-        <li>Wolf Guard Terminators</li>
-        <li>Grey Hunters II</li>
-        <li>Njal Stormcaller</li>
-        <li>Krom Dragongaze</li>
-        <li>Arjac Rockfist</li>
-        <li>Logan Grimnar</li>
-        <li>Ragnar Blackmane</li>
-        <li>Hunter/Stalker</li>
-        <li>Stormbird</li>
-      </ul>
-
-      <h3>Inquisition</h3>
-      <ul>
-        <li>Inquisitor Coteaz</li>
-        <li>Assassins</li>
-      </ul>
-
-      <h3>Orks</h3>
-      <ul>
-        <li>Grukk Face-Rippa</li>
-        <li>Ork Nobz</li>
-        <li>Killa Kans</li>
-        <li>Runtherd</li>
-        <li>Ghazghkull Thraka</li>
-        <li>Ork Nobz</li>
-        <li>Ork Meganobz</li>
-        <li>Ammo Runt</li>
-        <li>Grot Oiler</li>
-      </ul>
-
-      <h3>Harlequins</h3>
-      <ul>
-        <li>Solitaire</li>
-        <li>Shadowseer</li>
-        <li>Death Jester</li>
-      </ul>
-
-      <h3>Dark Angels</h3>
-      <ul>
-        <li>Company Master</li>
-        <li>Librarian</li>
-        <li>Chaplain Seraphicus</li>
-        <li>Death Wing Terminators</li>
-        <li>Tactical Marines</li>
-        <li>Ravenwing Bikes</li>
-      </ul>
-
-      <h3>Death Guard</h3>
-      <ul>
-        <li>Lord of Contagion</li>
-        <li>Plague Marines</li>
-        <li>Foetid Bloat-drone</li>
-        <li>Death Guard Poxwalkers</li>
-        <li>Helbrute</li>
-        <li>Chaos Lord Kranon the Relentless</li>
-        <li>Chosen Draznicht's Ravagers</li>
-        <li>Cultists</li>
-        <li>Cultists II</li>
-      </ul>
-
-      <h2>Age of Sigmar</h2>
+      <p>
+        Canis Wolfborn | Wolves | Geigor Fell-Hand | Wolf Guard Swiftclaws |
+        Thunderwolves | Grey Hunters
+      </p>
 
       <h3>Gloomspite Gitz</h3>
-      <ul>
-        <li>Skragrott</li>
-      </ul>
-
-      <h3>Malign Sorcery</h3>
-      <ul>
-        <li>Purple Sun of Shyish</li>
-        <li>Ravenak’s Gnashing Jaws</li>
-        <li>Suffocating Gravetide</li>
-        <li>Quicksilver Swords</li>
-        <li>Aethervoid Pendulun</li>
-        <li>Chronomantic Cogs</li>
-        <li>Umbral Spellportal</li>
-        <li>Soulsnare Shackles</li>
-        <li>Geminids of Uhl-Ghysh</li>
-        <li>Burning Head</li>
-        <li>Malevolent Maelstrom</li>
-        <li>Prismatic Palisade</li>
-        <li>Emerald Lifeswarm</li>
-      </ul>
-
-      <h2>Board Games</h2>
-
-      <h3>Execution Force</h3>
-      <ul>
-        <li>Callidus</li>
-        <li>Cullexus</li>
-        <li>Eversor</li>
-        <li>Vindicare</li>
-        <li>Chaos Terminator Lord</li>
-        <li>Chaos Space Marines</li>
-        <li>Chaos Cultists</li>
-      </ul>
-
-      <h3>Deathwatch Overkill</h3>
-      <ul>
-        <li>Chaplain Cassius</li>
-        <li>Deathwatch Members</li>
-        <li>Patriarch</li>
-        <li>Magus</li>
-        <li>Primus</li>
-        <li>Familiars </li>
-        <li>Acolyte Hybrids</li>
-        <li>Neophyte Cultists</li>
-        <li>Genestalers </li>
-        <li>Genestealer Aberrants</li>
-      </ul>
-
-      <h3>Space Hulk</h3>
-      <ul>
-        <li>Space Marine Terminators</li>
-        <li>Genestealers</li>
-        <li>Broodlord</li>
-        <li>Artefact</li>
-        <li>Dead Space Marine</li>
-        <li>C.A.T</li>
-      </ul>
-
-      <h3>Lost Patrol</h3>
-      <ul>
-        <li>Space Marine Scouts</li>
-        <li>Genestealers</li>
-        <li>Infestations</li>
-      </ul>
-
-      <h3>Underworlds</h3>
-      <ul>
-        <li>Zarbag's Gitz</li>
-        <li>Stormsire's Cursebreakers</li>
-        <li>Thorns of the Briar Queen</li>
-      </ul>
-
-      <h3>Burning of Prospero</h3>
-      <ul>
-        <li>Ahzek Ahriman</li>
-        <li>Geigor Fell-Hand</li>
-        <li>Tartaros Terminators</li>
-        <li>Legion Veterans</li>
-        <li>Legion Veterans II</li>
-        <li>Legion Veterans III</li>
-        <li>Custodian Guard</li>
-        <li>Sisters of Silence</li>
-      </ul>
-
-      <h2>Scenery</h2>
-
-      <h3>Age of Sigmar Objectives</h3>
-      <ul>
-        <li>Hallowed Tomb</li>
-        <li>Trove of Arcane Glory</li>
-        <li>Soul Stone</li>
-        <li>The Realm’s Ransom</li>
-        <li>Ensorcelled Armoury</li>
-        <li>Iconoclast Axe</li>
-        <li>Realmvault Key</li>
-      </ul>
-
-      <h3>Wall of Martyrs</h3>
-      <ul>
-        <li>Aquila Strongpoint</li>
-        <li>Firestorm Redoubt</li>
-        <li>Firestorm Redoubt</li>
-        <li>Vengeance Weapon Battery</li>
-        <li>Imperial Bunker</li>
-        <li>Imperial Defence Line</li>
-        <li>Imperial Defence Emplacement</li>
-      </ul>
-
-      <h2>Completed projects without posts:</h2>
-
-      <h3>Scenery</h3>
-      <ul>
-        <li>Corridor Ends</li>
-        <li>Corridor 1 Tiles</li>
-        <li>Corridor 2 Tiles</li>
-        <li>Corridor 3 Tiles</li>
-        <li>Corridor 4 Tiles</li>
-        <li>Corridor Corners</li>
-        <li>Corridor Intersections</li>
-        <li>Corridor Bifurcations</li>
-        <li>Corridor Special Intersection</li>
-        <li>Corridor Wide 4 Tiles</li>
-        <li>1 Access 3x3 Room</li>
-        <li>2 Access 3x3 Room L</li>
-        <li>2 Access 3x3 Room</li>
-        <li>3 Access 3x3 Room</li>
-        <li>4 Access 3x3 Room</li>
-        <li>1 Access 3x4 Room</li>
-        <li>Divided Stairs</li>
-        <li>4 Tiles Catwalk</li>
-        <li>Continuous Stairs</li>
-        <li>Octagon Platform</li>
-      </ul>
-
-      <h3>Space Wolves</h3>
-      <ul>
-        <li>Canis Wolfborn</li>
-        <li>Wolves</li>
-        <li>Geigor Fell-Hand</li>
-        <li>Wolf Guard Swiftclaws</li>
-        <li>Thunderwolves</li>
-        <li>Grey Hunters</li>
-      </ul>
-
-      <h3>Gloomspite Gitz</h3>
-      <ul>
-        <li>Throgg</li>
-      </ul>
+      <p>Throgg</p>
 
       <h3>Dreadfleet</h3>
-      <ul>
-        <li>Dragon Auxiliary</li>
-        <li>Dirigible Auxiliary</li>
-        <li>Islands</li>
-        <li>Shipwreck Pieces</li>
-        <li>Sea Monsters</li>
-        <li>Treasure Tokens</li>
-        <li>Wind Gauge</li>
-        <li>Ship's Wheels</li>
-        <li>Navigation Rod</li>
-        <li>The Bloody Reaver</li>
-        <li>Skabrus</li>
-        <li>Curse of Zandri</li>
-        <li>Black Kraken</li>
-        <li>Shadewraith</li>
-        <li>The Heldenhammer</li>
-        <li>The Swordfysh</li>
-        <li>The Flaming Scimitar</li>
-        <li>Grimnir's Thunder</li>
-        <li>Seadrake</li>
-        <li>Cog Auxiliaries</li>
-      </ul>
+      <p>
+        Dragon Auxiliary | Dirigible Auxiliary | Islands | Shipwreck Pieces |
+        Sea Monsters | Treasure Tokens | Wind Gauge | Ship's Wheels | Navigation
+        Rod | The Bloody Reaver | Skabrus | Curse of Zandri | Black Kraken |
+        Shadewraith | The Heldenhammer | The Swordfysh | The Flaming Scimitar |
+        Grimnir's Thunder | Seadrake | Cog Auxiliaries
+      </p>
 
       <h3>Gorechosen</h3>
-      <ul>
-        <li>Exalted Deathbringer</li>
-        <li>Skullgrinder</li>
-        <li>Slaughterpriest</li>
-        <li>Aspiring Deathbringer</li>
-        <li>Slaughterpriest</li>
-      </ul>
+      <p>
+        Exalted Deathbringer | Skullgrinder | Slaughterpriest | Aspiring
+        Deathbringer | Slaughterpriest
+      </p>
 
       <h3>Silver Tower</h3>
-      <ul>
-        <li>Knight Questo</li>
-        <li>Darkoeth Chieftan</li>
-        <li>Fyreslayer Doomseeker</li>
-        <li>Excelsior War Priest</li>
-        <li>Mistweaver Saith</li>
-        <li>Tenebrand Shard</li>
-        <li>Gaunt Summoner</li>
-        <li>Ogroid Traumaturge</li>
-        <li>Skaven Deathrunners</li>
-        <li>Familiars</li>
-        <li>Tzaangors </li>
-        <li>Pink Horrors</li>
-        <li>Blue Horrors</li>
-      </ul>
+      <p>
+        Knight Questor | Darkoeth Chieftan | Fyreslayer Doomseeker | Excelsior
+        War Priest | Mistweaver Saith | Tenebrand Shard | Gaunt Summoner |
+        Ogroid Traumaturge | Skaven Deathrunners | Familiars | Tzaangors | Pink
+        Horrors | Blue Horrors
+      </p>
+
+      <h3>Scenery</h3>
+      <p>
+        Corridor Ends | Corridor 1 Tiles | Corridor 2 Tiles | Corridor 3 Tiles |
+        Corridor 4 Tiles | Corridor Corners | Corridor Intersections | Corridor
+        Bifurcations | Corridor Special Intersection | Corridor Wide 4 Tiles | 1
+        Access 3x3 Room | 2 Access 3x3 Room L | 2 Access 3x3 Room | 3 Access 3x3
+        Room | 4 Access 3x3 Room | 1 Access 3x4 Room | Divided Stairs | 4 Tiles
+        Catwalk | Continuous Stairs | Octagon Platform
+      </p>
+
+      <h2>Painting</h2>
+      <hr />
+
+      <h3>Gloomspite Gitz</h3>
+      <p>Skragrott</p>
+
+      <h2>Assembled</h2>
+      <hr />
+
+      <h3>Grey Knights</h3>
+      <p>
+        Imperial Knight | Kaldor Draigo | Grey Knight Terminators Squad II |
+        Grey Knight Paladins | Land Raider | Grand Master Voldus
+      </p>
+
+      <h3>Space Wolves</h3>
+      <p>
+        Centurions | Wolf Guard Terminators | Blood Claws | Primaris Captain |
+        Primaris Captain | Primaris Judicar | Primaris Bladeguard Ancient|
+        Primaris Intercessor Squad | Primaris Inceptor Squad | Primaris
+        Hellblaster Squad | Primaris Infiltrators | Wolf Guard Terminators |
+        Grey Hunters II | Njal Stormcaller | Krom Dragongaze | Arjac Rockfist |
+        Logan Grimnar | Ragnar Blackmane | Hunter/Stalker | Stormbird
+      </p>
+
+      <h3>Inquisition</h3>
+      <p>Inquisitor Coteaz | Assassins</p>
+
+      <h3>Orks</h3>
+      <p>
+        Grukk Face-Rippa | Ork Nobz | Kans | Runtherd | Ghazghkull Thraka | Ork
+        Nobz | Ork Meganobz | Ammo Runt | Grot Oiler
+      </p>
+
+      <h3>Harlequins</h3>
+      <p>Solitaire | Shadowseer | Death Jester</p>
+
+      <h3>Dark Angels</h3>
+      <p>
+        Company Master | Librarian | Chaplain Seraphicus | Death Wing
+        Terminators | Tactical Marines | Ravenwing Bikes
+      </p>
+
+      <h3>Death Guard</h3>
+      <p>
+        Lord of Contagion | Plague Marines | Foetid Bloat-drone | Death Guard
+        Poxwalkers | Helbrute | Chaos Lord Kranon the Relentless | Chosen
+        Draznicht's Ravagers | Cultists | Cultists II
+      </p>
+
+      <h3>Malign Sorcery</h3>
+      <p>
+        Purple Sun of Shyish | Ravenak’s Gnashing Jaws | Suffocating Gravetide |
+        Quicksilver Swords | Aethervoid Pendulun | Chronomantic Cogs | Umbral
+        Spellportal | Soulsnare Shackles | Geminids of Uhl-Ghysh | Burning Head
+        | Malevolent Maelstrom | Prismatic Palisade | Emerald Lifeswarm
+      </p>
+
+      <h3>Execution Force</h3>
+      <p>
+        Callidus | Cullexus | Eversor | Vindicare | Chaos Terminator Lord |
+        Chaos Space Marines | Chaos Cultists
+      </p>
+
+      <h3>Deathwatch Overkill</h3>
+      <p>
+        Chaplain Cassius | Deathwatch Members | Patriarch | Magus | Primus |
+        Familiars | Acolyte Hybrids | Neophyte Cultists | Genestealers |
+        Genestealer Aberrants
+      </p>
+
+      <h3>Space Hulk</h3>
+      <p>
+        Space Marine Terminators | Genestealers | Broodlord | Artefact | Dead
+        Space Marine | C.A.T
+      </p>
+
+      <h3>Lost Patrol</h3>
+      <p>Space Marine Scouts | Genestealers | Infestations</p>
+
+      <h3>Underworlds</h3>
+      <p>
+        Zarbag's Gitz | Stormsire's Cursebreakers | Thorns of the Briar Queen
+      </p>
+
+      <h3>Burning of Prospero</h3>
+      <p>
+        Ahzek Ahriman | Geigor Fell-Hand | Tartaros Terminators | Legion
+        Veterans | Legion Veterans II | Legion Veterans III | Custodian Guard |
+        Sisters of Silence
+      </p>
+
+      <h3>Age of Sigmar Objectives</h3>
+      <p>
+        Hallowed Tomb | Trove of Arcane Glory | Soul Stone | The Realm’s Ransom
+        | Ensorcelled Armoury | Iconoclast Axe | Realmvault Key
+      </p>
+
+      <h3>Wall of Martyrs</h3>
+      <p>
+        Aquila Strongpoint | Firestorm Redoubt | Firestorm Redoubt | Vengeance
+        Weapon Battery | Imperial Bunker | Imperial Defence Line | Imperial
+        Defence Emplacement
+      </p>
     </Layout>
   );
 };
