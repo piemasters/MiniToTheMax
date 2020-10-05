7ad0f83f6126c4005c18fa5aa0cd3b5bad31a8a5
David Norton <pie_masters@hotmail.co.uk>
2020-10-05 17:34:27 +0100

    feat: Added shades

---
+++ src/data/paints/shade.tsx
@@ -0,0 +1,184 @@
+import { PaintDetails } from '../../types/app.types';
+
+export const shadeCarroburgCrimson: PaintDetails = {
+  name: 'Carroburg Crimson',
+  type: 'shade',
+  color: 'red',
+  hex: '#310808',
+  gradient: [
+    { offset: 0, color: 'rgb(49, 8, 8)' },
+    { offset: 100, color: 'rgb(255, 183, 235)' },
+  ],
+};
+
+export const shadeNulnOil: PaintDetails = {
+  name: 'Nuln Oil',
+  type: 'shade',
+  color: 'black',
+  hex: '#101010',
+  gradient: [
+    { offset: 0, color: 'rgb(16, 16, 16)' },
+    { offset: 100, color: 'rgb(223, 217, 214)' },
+  ],
+};
+
+export const shadeAgraxEarthshade: PaintDetails = {
+  name: 'Agrax Earthshade',
+  type: 'shade',
+  color: 'brown',
+  hex: '#2D190C',
+  gradient: [
+    { offset: 0, color: 'rgb(45, 25, 12)' },
+    { offset: 100, color: 'rgb(226, 218, 200)' },
+  ],
+};
+
+export const shadeDruchiiViolet: PaintDetails = {
+  name: 'Druchii Violet',
+  type: 'shade',
+  color: 'purple',
+  hex: '#250B27',
+  gradient: [
+    { offset: 0, color: 'rgb(37, 11, 39)' },
+    { offset: 100, color: 'rgb(191, 142, 195)' },
+  ],
+};
+
+export const shadeDrakenhofNightshade: PaintDetails = {
+  name: 'Drakenhof Nightshade',
+  type: 'shade',
+  color: 'blue',
+  hex: '#0A131B',
+  gradient: [
+    { offset: 0, color: 'rgb(10, 19, 27)' },
+    { offset: 100, color: 'rgb(230, 232, 243)' },
+  ],
+};
+
+export const shadeCasandoraYellow: PaintDetails = {
+  name: 'Casandora Yellow',
+  type: 'shade',
+  color: 'yellow',
+  hex: '#E78035',
+  gradient: [
+    { offset: 0, color: 'rgb(231, 128, 53)' },
+    { offset: 100, color: 'rgb(252, 239, 188)' },
+  ],
+};
+
+export const shadeBielTanGreen: PaintDetails = {
+  name: 'Biel Tan Green',
+  type: 'shade',
+  color: 'green',
+  hex: '#132E21',
+  gradient: [
+    { offset: 0, color: 'rgb(19, 46, 33)' },
+    { offset: 100, color: 'rgb(174, 220, 162)' },
+  ],
+};
+
+export const shadeFueganOrange: PaintDetails = {
+  name: 'Fuegan Orange',
+  type: 'shade',
+  color: 'orange',
+  hex: '#6C1C03',
+  gradient: [
+    { offset: 0, color: 'rgb(108, 28, 3)' },
+    { offset: 100, color: 'rgb(223, 159, 104)' },
+  ],
+};
+
+export const shadeAthonianCamoshade: PaintDetails = {
+  name: 'Athonian Camoshade',
+  type: 'shade',
+  color: 'green',
+  hex: '#1C1C10',
+  gradient: [
+    { offset: 0, color: 'rgb(28, 28, 16)' },
+    { offset: 100, color: 'rgb(253, 254, 191)' },
+  ],
+};
+
+export const shadeCoeliaGreenshade: PaintDetails = {
+  name: 'Coelia Greenshade',
+  type: 'shade',
+  color: 'green',
+  hex: '#113431',
+  gradient: [
+    { offset: 0, color: 'rgb(17, 52, 49)' },
+    { offset: 100, color: 'rgb(32, 184, 172)' },
+  ],
+};
+
+export const shadeSeraphimSepia: PaintDetails = {
+  name: 'Seraphim Sepia',
+  type: 'shade',
+  color: 'brown',
+  hex: '#2E1F08',
+  gradient: [
+    { offset: 0, color: 'rgb(46, 31, 8)' },
+    { offset: 100, color: 'rgb(233, 174, 125)' },
+  ],
+};
+
+export const shadeReiklandFleshshade: PaintDetails = {
+  name: 'Reikland Fleshshade',
+  type: 'shade',
+  color: 'flesh',
+  hex: '#311908',
+  gradient: [
+    { offset: 0, color: 'rgb(49, 25, 8)' },
+    { offset: 100, color: 'rgb(255, 213, 183)' },
+  ],
+};
+
+export const shadeNulnOilGloss: PaintDetails = {
+  name: 'Nuln Oil Gloss',
+  type: 'shade',
+  color: 'black',
+  hex: '#101010',
+  gradient: [
+    { offset: 0, color: 'rgb(16, 16, 16)' },
+    { offset: 100, color: 'rgb(223, 217, 214)' },
+  ],
+};
+
+export const shadeAgraxEarthshadeGloss: PaintDetails = {
+  name: 'Agrax Earthshade Gloss',
+  type: 'shade',
+  color: 'brown',
+  hex: '#2D190C',
+  gradient: [
+    { offset: 0, color: 'rgb(45, 25, 12)' },
+    { offset: 100, color: 'rgb(226, 218, 200)' },
+  ],
+};
+
+export const shadeReiklandFleshshadeGloss: PaintDetails = {
+  name: 'Reikland Fleshshade Gloss',
+  type: 'shade',
+  color: 'flesh',
+  hex: '#311908',
+  gradient: [
+    { offset: 0, color: 'rgb(49, 25, 8)' },
+    { offset: 100, color: 'rgb(255, 213, 183)' },
+  ],
+};
+
+export const shadePaints = [
+  shadeCarroburgCrimson,
+  shadeNulnOil,
+  shadeAgraxEarthshade,
+  shadeDruchiiViolet,
+  shadeDrakenhofNightshade,
+  shadeCasandoraYellow,
+  shadeBielTanGreen,
+  shadeFueganOrange,
+  shadeAthonianCamoshade,
+  shadeCoeliaGreenshade,
+  shadeSeraphimSepia,
+  shadeReiklandFleshshade,
+  shadeNulnOilGloss,
+  shadeAgraxEarthshadeGloss,
+  shadeReiklandFleshshadeGloss,
+];
