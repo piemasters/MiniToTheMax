acc9584fa814040a3f405527c6ef6b2dadc8f99e
David Norton <pie_masters@hotmail.co.uk>
2021-02-06 10:46:34 +0000

    feat: posts now have paints

---
+++ content/components/paint.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import BasePaint from '../../src/components/paint';
+import { airPaints } from '../../src/data/paints/air';
+import { basePaints } from '../../src/data/paints/base';
+import { contrastPaints } from '../../src/data/paints/contrast';
+import { dryPaints } from '../../src/data/paints/dry';
+import { layerPaints } from '../../src/data/paints/layer';
+import { shadePaints } from '../../src/data/paints/shade';
+import { sprayPaints } from '../../src/data/paints/spray';
+import { technicalPaints } from '../../src/data/paints/technical';
+import { PaintDetails } from '../../src/types/app.types';
+import { css } from '@emotion/react';
+
+const allPaints = [
+  ...basePaints,
+  ...layerPaints,
+  ...shadePaints,
+  ...contrastPaints,
+  ...dryPaints,
+  ...sprayPaints,
+  ...technicalPaints,
+  ...airPaints,
+];
+
+const Paint = ({ name, type }: { name: String; type?: String }) => {
+  const paintWrapperStyles = css`
+    display: inline-block;
+    margin-bottom: 12px;
+  `;
+
+  const paintMatches: PaintDetails[] = allPaints.filter(
+    (paint) => paint.name === name
+  );
+
+  const paint: PaintDetails = type
+    ? paintMatches.filter((paint) => paint.type === type)[0]
+    : paintMatches[0];
+
+  return (
+    <div css={paintWrapperStyles}>
+      <BasePaint paint={paint} />
+    </div>
+  );
+};
+
+export default Paint;
