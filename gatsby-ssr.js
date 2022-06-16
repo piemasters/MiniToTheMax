a43ec467a0686ee7cdec6c6b1949d6157b7bf33e
David Norton <pie_masters@hotmail.co.uk>
2022-06-16 15:11:37 +0100

    feat: add google analytics

---
+++ gatsby-ssr.js
@@ -0,0 +1,9 @@
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3368959360128868"
+      crossOrigin="anonymous"
+    ></script>,
+  ]);
+};
