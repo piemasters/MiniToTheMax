1333ef69bf02e8cf051493f3863d3b8042faeffa
David Norton <pie_masters@hotmail.co.uk>
2023-01-01 01:12:04 +0000

    update gloomspite gitz backlog list

--- src/data/backlog/age-of-sigmar/gloomspite-gitz.ts
+++ src/data/backlog/age-of-sigmar/gloomspite-gitz.ts
@@ -1,15 +1,36 @@
 export const gloomspiteGitz = [
   {
+    name: 'Bazdrogg Nekk-choppa',
+  },
+  {
     name: 'Da Red Gobbo',
     link: '/showcase/gloomspite-gitz/da-red-gobbo/',
+  },
+  {
+    name: 'Loonboss on Giant Cave Squig',
   },
   {
     name: 'Mangler Squigs',
     link: '/showcase/gloomspite-gitz/mangler-squigs/',
   },
   {
+    name: 'Mangler Squigs II',
+  },
+  {
     name: 'Morglum Necksnapper',
     link: '/showcase/gloomspite-gitz/morglum-necksnapper/',
+  },
+  {
+    name: 'Squig Gobba',
+  },
+  {
+    name: 'Squig Herd',
+  },
+  {
+    name: 'Squig Herd II',
+  },
+  {
+    name: 'Squig Hoppers',
   },
   {
     name: 'Skarsnik & Gobbla',
