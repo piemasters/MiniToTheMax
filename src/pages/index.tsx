4c54a059a27ad29919d9747544c50ffa69005739
David Norton <pie_masters@hotmail.co.uk>
2023-04-26 17:33:31 +0100

    feat: update latest video to colossal squig

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,8 +44,8 @@ const IndexPage = (): React.ReactNode => {
           <Badges />
           <h2>Latest Video</h2>
           <Video
-            src="https://www.youtube.com/embed/ZsBtMCPGnkU"
-            title="Blackstone Fortress: Heroes"
+            src="https://www.youtube.com/embed/QgXNweWR7RQ"
+            title="Gloomspite Gitz: Colossal Squig"
           />
           <h2>Latest Post</h2>
 
