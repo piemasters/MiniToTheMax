9e81bb2b1e7e60821cfcb1f18a6784c847138f61
David Norton <pie_masters@hotmail.co.uk>
2025-02-21 18:20:32 +0000

    feat: squidmar essential post live

--- src/data/backlog/other/squidmar.ts
+++ src/data/backlog/other/squidmar.ts
@@ -3,7 +3,10 @@ export const squidmar = [
     name: 'Dark Future Paint Set',
     link: '/showcase/squidmar/dark-future-paint-set/',
   },
-  { name: 'Essential Paint Set' },
+  {
+    name: 'Essential Paint Set',
+    link: '/showcase/squidmar/essential-paint-set/',
+  },
   {
     name: 'Fantasy Paint Set',
     link: '/showcase/squidmar/fantasy-paint-set/',
